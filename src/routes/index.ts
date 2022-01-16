7520c1daaecf572d36ede25878089d05007b0843
Renato Tiago Samarrenha <renatosamarrenha@gmail.com>
2022-01-16 20:11:42 -0300

    base criada

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import {Router} from 'express'
+import * as PageControler from '../controllers/pageControllers'
+import * as SearchControler from '../controllers/searchController'
 
 const router = Router()
 
-router.get('/', (req, res)=>{
-    res.send('home')
+router.get('/', PageControler.home)
+router.get('/dogs', PageControler.dogs)
+router.get('/cats', PageControler.cats)
+router.get('/fishes', PageControler.fishes)
 
-})
+router.get('/search', SearchControler.search)
 
 export default router
